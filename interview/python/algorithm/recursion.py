097adf3d845a3224aeb855413997dcc71bf79bc7
hcqgreat <hcq186@163.com>
2020-09-03 22:54:53 +0800

    update

--- interview/python/algorithm/recursion.py
+++ interview/python/algorithm/recursion.py
@@ -2,64 +2,66 @@
 #  coding : utf-8
 #  auther : Hu Chengqiang
 
-class Recursion():
-    '''
+
+class RecursionClass:
+    """
         递归算法----->自己调用自己
         递归结构包括两个部分:
          定义递归头。解答:什么时候不调用自身方法。如果没有头，将陷入死循环，也就是递归的结束条件。
          递归体。解答:什么时候需要调用自身方法。
-    '''
+    """
 
     def factorial(self, n):
-        '''
-        利用递归算法，计算n的阶乘
-        :param n:
-        :return: n!
-        '''
-        if n == 1 :
+        """
+            利用递归算法，计算n的阶乘
+            :param n:
+            :return:
+        """
+
+        if n == 1:
             return 1
         else:
             return n * self.factorial(n - 1)
 
-    def fibonacci_sequence(self, n):
-        '''
-        斐波那契数列
-        :return: 数列
-        '''
-
-        list = []
+    def fibonacci_sequences(self, n):
+        """
+            斐波那契数列
+            :param n:
+            :return: 数列
+        """
+        lst = []
         a, b = 0, 1
         while a < n:
-            list.append(a)
-            a, b = b, a +b
-        return list
+            lst.append(a)
+            a, b = b, a + b
+        return lst
 
-
-    def getAllFiles(self, path, level):
-        '''
-        利用递归，展示目录树
-        :param path:
-        :return: 目录树
-        '''
+    def get_all_files(self, path, level):
+        """
+            利用递归，展示目录树
+            :param path:
+            :param level:
+            :return: 目录树
+        """
         import os
-        childFiles = os.listdir(path)
-        allFiles = []
-        for file in childFiles:
+        child_files = os.listdir(path)
+        all_files = []
+        for file in child_files:
             filepath = os.path.join(path,file)
             if os.path.isdir(filepath):
-                self.getAllFiles(filepath,level + 1)
-            allFiles.append("\t" * level + filepath)
+                self.get_all_files(filepath,level + 1)
+            all_files.append("\t" * level + filepath)
 
-        for f in reversed(allFiles):
+        for f in reversed(all_files):
             print(f)
 
 
 if __name__ == '__main__':
-    recursion = Recursion()
-    #test = recursion.factorial(5)
-    #print(test)
+    recursion = RecursionClass()
+    # test = recursion.factorial(5)
+    # print(test)
 
     # recursion.getAllFiles("/Users/work/code/python/Pythonbase/",0)
 
-    list = recursion.fibonacci_sequence(100)
-    print(list)
+    lst = recursion.fibonacci_sequences(100)
+    print(lst)
--- interview/python/design_patterns/singleton.py
+++ interview/python/design_patterns/singleton.py
@@ -1,3 +1,40 @@
 #! env/bin/python
 #  coding : utf-8
 #  auther : Hu Chengqiang
+
+
+class MySingletonOneClass:
+    """
+        单例设计模式  ----> 第一种
+        单例模式(Singleton Pattern)的核心作用是确保一个类只有一个实例，并且提供一 个访问该实例的全局访问点
+    """
+
+    __obj = None
+    __init_flag = True
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__obj:
+            cls.__obj = super().__new__(cls)
+        return cls.__obj
+
+    def __init__(self, name):
+        if MySingletonOneClass.__init_flag:
+            print("init")
+            self.name = name
+            MySingletonOneClass.__init_flag = False
+
+
+class MySingletonTwoClass:
+    """
+        单例设计模式 ----> 第二种
+    """
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            org = super(MySingletonTwoClass, cls)
+            cls._instance = org.__new__(cls)
+
+        return cls._instance
+
+
+singleton_one = MySingletonOneClass('test')
